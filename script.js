32beb46a0964bb30dbd0b972968a77ba37ce2411
Akileus77 <Akikami77@gmail.com>
2020-12-11 10:54:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+    document.querySelector(".toggleButton").addEventListener("click", toggleDarKMode);
+function toggleDarKMode() {
+   var element = document.body;
+   element.classList.toggle("bg-dark");
+}
